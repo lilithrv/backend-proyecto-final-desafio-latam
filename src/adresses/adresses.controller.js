a50df9def6d97e7a2f883caf1b42453f96abbb32
Matt Cuadros <mcuadrose@gmail.com>
2023-07-21 01:09:39 -0400

    feat: modifyAddress added

--- src/adresses/adresses.controller.js
+++ src/adresses/adresses.controller.js
@@ -8,8 +8,8 @@ const getAddress = async (req, res) => {
     console.log(result);
     return res.status(200).json({ ok: true, result });
   } catch (error) {
-    const { status, message } = handleErrors(error.code)
-    console.log(error, message)
+    const { status, message } = handleErrors(error.code);
+    console.log(error, message);
     return res.status(status).json({ ok: false, result: message });
   }
 };
@@ -25,13 +25,34 @@ const newAddress = async (req, res) => {
     console.log(result);
     return res.status(201).json({ ok: true, result });
   } catch (error) {
-    const { status, message } = handleErrors(error.code)
-        console.log(error, message)
-        return res.status(status).json({ ok: false, result: message });
+    const { status, message } = handleErrors(error.code);
+    console.log(error, message);
+    return res.status(status).json({ ok: false, result: message });
+  }
+};
+
+const modifyAddress = async (req, res) => {
+  try {
+    const { address, commune_id } = req.body;
+    const { user_id } = req.params;
+    const result = await addressesModel.updateAddress(
+      address,
+      commune_id,
+      user_id
+    );
+    console.log(result);
+    return res
+      .status(201)
+      .json({ ok: true, message: "La dirección fue modificada", result });
+  } catch (error) {
+    const { status, message } = handleErrors(error.code);
+    console.log(error, message);
+    return res.status(status).json({ ok: false, result: message });
   }
 };
 
 export const addressController = {
   getAddress,
   newAddress,
+  modifyAddress,
 };
--- src/adresses/adresses.route.js
+++ src/adresses/adresses.route.js
@@ -4,6 +4,7 @@ import { addressController } from "./adresses.controller.js";
 const router = Router();
 
 router.get("/user/:user_id/addresses", addressController.getAddress);
+router.put("/user/:user_id/addresses", addressController.modifyAddress);
 router.post("/addresses", addressController.newAddress);
 
 export default router;
